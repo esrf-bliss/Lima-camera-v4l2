1965d13bb1ad392bec617bb3a39b15bfcbeed9e1
Samuel Debionne <samuel.debionne@esrf.fr>
2018-08-03 14:32:00 +0200

    Update the build sytem to support devel conda package

---
+++ cmake/FindV4L2.cmake
@@ -0,0 +1,12 @@
+set(V4L2_INCLUDE_DIRS)
+set(V4L2_LIBRARIES)
+set(V4L2_DEFINITIONS)
+
+find_path(V4L2_INCLUDE_DIRS "libv4l2.h")
+find_library(V4L2_LIBRARIES v4l2)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Andor DEFAULT_MSG
+  V4L2_LIBRARIES
+  V4L2_INCLUDE_DIRS
+)
